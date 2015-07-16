499bcd77d98e6ef3782d420d2fa1cc66b99e8534
Andrew Olson <olson@cshl.edu>
2015-07-16 11:34:44 -0400

    fixed the formatting lots

--- scripts/components/resultDetails/xrefs.jsx
+++ scripts/components/resultDetails/xrefs.jsx
@@ -3,6 +3,78 @@ var _ = require('lodash');
 var bs = require('react-bootstrap');
 var dbxrefs = require('gramene-dbxrefs');
 
+var HOW_MANY_TO_SHOW_BY_DEFAULT = 10;
+
+var Xref = React.createClass({
+  propTypes: {
+    displayName: React.PropTypes.string.isRequired,
+    members: React.PropTypes.array.isRequired
+  },
+  
+  getInitialState: function() {
+    return {showAll: false}
+  },
+  
+  toggleShowAll: function() {
+    this.setState({showAll: !this.state.showAll});
+  },
+  
+  possiblyTruncateList: function(vals) {
+    var ellipsis, ellipsisChar, ellipsisTitle;
+    
+    if(vals.length > HOW_MANY_TO_SHOW_BY_DEFAULT) {
+      if(this.state.showAll) {
+        ellipsisChar = '^ show first ' + HOW_MANY_TO_SHOW_BY_DEFAULT;
+        ellipsisTitle = 'Show less';
+      }
+      else {
+        ellipsisChar = '… show all (' + (vals.length - HOW_MANY_TO_SHOW_BY_DEFAULT) + ' more)';
+        ellipsisTitle = 'Show more';
+        vals = vals.slice(0, HOW_MANY_TO_SHOW_BY_DEFAULT);
+      }
+      
+      ellipsis = (
+        <li key="showMore" className="showAll"><a title={ellipsisTitle} onClick={this.toggleShowAll}>{ellipsisChar}</a></li>
+      );
+      
+      vals.push(ellipsis);
+    }
+    
+    return vals;
+  },
+  
+  render: function() {
+    var members, vals;
+    
+    members = this.props.members;
+    
+    vals = _(members)
+      .map(function(m) {
+        return m.val;
+      })
+      .flatten(true)
+      .sort()
+      .uniq(true) // TODO figure out why there are duplicates.
+      .map(function(item, idx) {
+        var url = members[0].url(item),
+            liClass = idx < HOW_MANY_TO_SHOW_BY_DEFAULT ? "default" : "extra";
+        return (
+          <li key={idx} className={liClass}><a href={url}>{item}</a></li>
+        )
+      })
+      .value();
+      
+    vals = this.possiblyTruncateList(vals);
+    
+    return (
+      <tr>
+        <td className="xref-name-col">{this.props.displayName}</td>
+        <td className="xref-value-col"><ol className="xref-id-list">{vals}</ol></td>
+      </tr>
+    );
+  }
+});
+
 module.exports = React.createClass({
 
   componentWillMount: function() {
@@ -13,34 +85,12 @@ module.exports = React.createClass({
     .map(function(val, name) {
       var xref = dbxrefs.fetch(name);
       return {url: xref.url, label: xref.label, val: val};
-      //   urls: val.map(function(item) {
-      //           var url = xref.url(item);
-      //             return (
-      //               <li><a href={url}>{item}</a></li>
-      //             );
-      //         })
-      // };
     })
     .groupBy('label')
     .map(function(members, displayName) {
-      var vals = [];
-      members.forEach(function(m) {
-        vals.push(m.val)
-      });
-      vals = _.flatten(vals,true).sort();
-      vals = _.uniq(vals,true)
-      .map(function(item) {
-        var url = members[0].url(item);
-        return (
-          <li><a href={url}>{item}</a></li>
-        )
-      });
       return (
-        <tr>
-          <td className="xref-name-col">{displayName}</td>
-          <td className="xref-value-col"><ol className="xref-id-list">{vals}</ol></td>
-        </tr>
-      );
+        <Xref displayName={displayName} members={members} />
+      )
     })
     .value();
   },
