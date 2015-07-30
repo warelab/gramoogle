84c1aa76cc587cd45784561b18706b74e7e399b9
mycrobe <jmulvane@cshl.edu>
2015-07-30 11:42:59 -0400

    show welcome screen on initial load

--- scripts/components/app.jsx
+++ scripts/components/app.jsx
@@ -3,38 +3,48 @@
 var React = require('react');
 var Reflux = require('reflux');
 var Header = require('./header.jsx');
-//var TextSearch = require('./search.jsx');
-//var Filters = require('./filters.jsx');
-//var SearchSummary = require('./searchSummary.jsx');
 var Results = require('./results.jsx');
+var Welcome = require('./welcome.jsx');
 var searchStore = require('../stores/searchStore');
+var QueryActions = require('../actions/queryActions');
+var _ = require('lodash');
 
 var App = React.createClass({
+
   mixins: [
     Reflux.connect(searchStore, 'search')
   ], // this mixin binds the store (where search/filter/results state lives) to this.state.search
+
+  getInitialState: function () {
+    return {
+      showResults: false, // don't show results initially
+    };
+  },
+
+  componentWillUpdate: function () {
+    // show results if we have already done that or if there are filters.
+    // (conversely, only show the welcome screen if the user has never
+    // supplied a filter.)
+    if(!this.state.showResults && !!_.size(this.state.search.query.filters)) {
+      this.setState({showResults: true});
+    }
+  },
+
   render: function () {
-    var search = this.state.search;
+    var search = this.state.search,
+      content;
+
+    content = this.state.showResults ?
+      <Results results={search.results}/> :
+      <Welcome/>
+    ;
 
     return (
       <div className="app container">
-        <Header search={search} />
-        <Results results={search.results} />
+        <Header search={search}/>
+        {content}
       </div>
     );
-    //
-    //return (
-    //  <div className="app">
-    //      <div class="container-fluid">
-    //        <div className="logo"> </div>
-    //        <TextSearch query={search.query} metadata={search.metadata} />
-    //        <SearchSummary filters={search.query.filters}
-    //                       results={search.results} />
-    //        <Filters query={search.query} results={search.results} />
-    //    </nav>
-    //    <Results results={search.results} />
-    //  </div>
-    //);
   }
 });
 
