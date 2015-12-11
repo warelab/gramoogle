1bc49294d7a965bf6f7e2195704bfabee35ac0ab
mycrobe <jmulvane@cshl.edu>
2015-12-11 16:21:35 -0500

    make the search and suggest components their own files.

---
+++ scripts/components/search/search.jsx
@@ -0,0 +1,86 @@
+'use strict';
+
+var React = require('react');
+var Reflux = require('reflux');
+var _ = require('lodash');
+
+var Nav = require('react-bootstrap').Nav;
+
+var QueryActions = require('../../actions/queryActions');
+
+var Suggest = require('../suggest/suggest.jsx');
+var SearchBox = require('./searchBox.jsx');
+var Filters = require('./filters.jsx');
+
+var Search = React.createClass({
+  mixins: [Reflux.ListenerMixin],
+  propTypes: {
+    search: React.PropTypes.object.isRequired,
+    onAnalysisButtonPress: React.PropTypes.func.isRequired
+  },
+  getInitialState: function() {
+    return {
+      suggestionsVisible: false
+    };
+  },
+  componentDidMount: function() {
+    // listen directly to an action method.
+
+    // Why?
+
+    // If we bind the search input's value to the query string state
+    // then it is updated when the search response comes back
+
+    // then it's really hard to use. So it's disconnected from the rest
+    // of app state and we must manually clear it here if the query string is
+    // removed (e.g. when a suggestion is picked)
+    this.listenTo(QueryActions.removeQueryString, this.clearInputString);
+  },
+  handleQueryChange: function(e) {
+    var queryString = e.target.value;
+
+    QueryActions.setQueryString(queryString);
+
+    // For now, show typeahead if query string is not empty
+    this.setState({
+      suggestionsVisible: !!queryString.length
+    });
+  },
+  clearInputString: function() {
+    this.refs.searchBox.clearSearchString();
+    this.setState({
+      suggestionsVisible: false
+    });
+  },
+  render: function(){
+    var suggestions,
+      filters,
+      search = this.props.search;
+
+    if(this.state.suggestionsVisible) {
+      suggestions = (
+        <Suggest queryString={this.props.search.query.q}/>
+      );
+    }
+
+    if(_.size(search.query.filters)) {
+      filters = (
+        <Filters filters={search.query.filters} />
+      );
+    }
+
+    return (
+      <Nav right={true}
+           className="search-box-nav">
+        <SearchBox ref="searchBox"
+                   results={search.results}
+                   onQueryChange={this.handleQueryChange}
+                   onStatsButtonPress={this.props.onAnalysisButtonPress} />
+        {filters}
+        {suggestions}
+      </Nav>
+    );
+  }
+});
+
+module.exports = Search;
---
+++ scripts/components/suggest/suggest.jsx
@@ -0,0 +1,67 @@
+'use strict';
+
+var React = require('react');
+var Reflux = require('reflux');
+var suggestStore = require('../../stores/suggestStore');
+var _ = require('lodash');
+
+var bs = require('react-bootstrap');
+
+var Category = require('./category.jsx');
+
+var Suggest = React.createClass({
+  mixins: [
+    Reflux.connect(suggestStore, 'suggestions')
+  ], // this mixin binds the store (where search/filter/results state lives) to this.state.suggest
+  propTypes: {
+    queryString: React.PropTypes.string.isRequired
+  },
+  render: function () {
+    var suggestionCategories = this.state.suggestions;
+
+    // if suggestionCategories is undefined, we haven't got our response back from the server yet.
+    if (!suggestionCategories) {
+      return (
+        <bs.Panel className="suggestions">
+          <p>Finding suggestions…</p>
+        </bs.Panel>
+      );
+    }
+
+    // if there is only one category, and it is flagged as "fullTextSearchOnly" (this is done in suggestStore#addQueryTerm)
+    // then we should warn the user that no real suggestions have been found and that they are probably SOL.
+    if(suggestionCategories.length === 1 && suggestionCategories[0].fullTextSearchOnly) {
+      return (
+        <bs.Panel className="suggestions">
+          <bs.Alert bsStyle="warning">
+            <strong>No suggestions found.</strong> You may still attempt a full text search, though it is unlikely to find any genes for you.
+          </bs.Alert>
+          <ul className="categories">
+            <SuggestCategory category={suggestionCategories[0]} hideLabel={true} />
+          </ul>
+        </bs.Panel>
+      )
+    }
+
+    var suggestLayout = _(suggestionCategories)
+      .filter(function (category) {
+        return !!category.suggestions.length;
+      })
+      .map(function (category) {
+        return (
+          <Category key={category.label} category={category}/>
+        );
+      })
+      .value();
+
+    return (
+      <bs.Panel className="suggestions">
+        <ul className="categories">
+          {suggestLayout}
+        </ul>
+      </bs.Panel>
+    );
+  }
+});
+
+module.exports = Suggest;
