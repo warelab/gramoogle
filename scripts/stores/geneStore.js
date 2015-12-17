b31c33af5afb2aad563152ca0a9591dfe4c6b70e
Joe Mulvaney <jmulvane@cshl.edu>
2015-12-17 12:54:21 -0500

    empty gene store

---
+++ scripts/stores/geneStore.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var Reflux = require('reflux');
+var _ = require('lodash');
+var Q = require('q');
+var taxonomyPromise = require('gramene-trees-client').promise;
+
+var GeneStore = Reflux.createStore({
+
+});
+
+module.exports = GeneStore;
