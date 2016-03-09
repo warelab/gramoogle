ef133bca7d068eb9b6764c065190b3251b4dd6d6
Joe Mulvaney <jmulvane@cshl.edu>
2016-03-09 17:21:17 -0500

    prototype for loading reactome images for #99. Doesn't work because no CORS headers on the server.

---
+++ scripts/components/result/details/pathways/reactomeImg.jsx
@@ -0,0 +1,39 @@
+'use strict';
+
+var React = require('react');
+var axios = require('axios');
+
+var urlTemplate = 'http://plantreactome.gramene.org/ReactomeRESTfulAPI/RESTfulWS/highlightPathwayDiagram/%pathwayId%/PNG';
+
+var ReactomeImg = React.createClass({
+  propTypes: {
+    pathwayId: React.PropTypes.number.isRequired
+  },
+  componentWillMount: function() {
+    var url = urlTemplate.replace('%pathwayId%', '' + this.props.pathwayId);
+    axios.post(url, {data: ''}).then(function(response) {
+      this.setState({dataURI: 'data:image/png;base64,' + response.data });
+    }.bind(this));
+
+  },
+  getInitialState: function() {
+    return {};
+  },
+  renderImg: function() {
+    var dataURI = this.state.dataURI;
+    if(dataURI) {
+      return (
+        <img src={this.state.dataURI} />
+      )
+    }
+  },
+  render: function() {
+    return (
+      <div className="reactomeImage">
+        {this.renderImg()}
+      </div>
+    );
+  }
+});
+
+module.exports = ReactomeImg;
