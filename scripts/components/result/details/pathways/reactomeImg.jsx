1c50b747eec11e5d245bfb8fc59c8519d35b3931
Joe Mulvaney <jmulvane@cshl.edu>
2016-03-18 16:44:55 -0400

    get xml metadata so we can ... do something with it?

--- scripts/components/result/details/pathways/reactomeImg.jsx
+++ scripts/components/result/details/pathways/reactomeImg.jsx
@@ -2,32 +2,40 @@
 
 var React = require('react');
 var axios = require('axios');
+var parseString = require('xml2js').parseString;
 
-var urlTemplate = 'http://plantreactomedev.oicr.on.ca/ReactomeRESTfulAPI/RESTfulWS/highlightPathwayDiagram/%pathwayId%/PNG';
+var urlTemplate = 'http://plantreactomedev.oicr.on.ca/ReactomeRESTfulAPI/RESTfulWS/highlightPathwayDiagram/%pathwayId%/';
 
 var ReactomeImg = React.createClass({
   propTypes: {
     pathwayId: React.PropTypes.number.isRequired
   },
-  componentWillMount: function() {
+  componentWillMount: function () {
     var url = urlTemplate.replace('%pathwayId%', '' + this.props.pathwayId);
-    axios.post(url, {data: ''}).then(function(response) {
-      this.setState({dataURI: 'data:image/png;base64,' + response.data });
+    axios.post(url + 'PNG', 'PP1S342_8V6.1', {headers: {'Content-Type': 'text/plain'}}).then(function (response) {
+      console.log(response);
+      this.setState({dataURI: 'data:image/png;base64,' + response.data});
+    }.bind(this));
+
+    axios.post(url + 'XML', 'PP1S342_8V6.1', {headers: {'Content-Type': 'application/xml'}}).then(function (response) {
+      parseString(response.data, function (err, result) {
+        this.setState({imageMetadata: result});
+      }.bind(this));
     }.bind(this));
 
   },
-  getInitialState: function() {
+  getInitialState: function () {
     return {};
   },
-  renderImg: function() {
+  renderImg: function () {
     var dataURI = this.state.dataURI;
-    if(dataURI) {
+    if (dataURI) {
       return (
-        <img src={this.state.dataURI} />
+        <img src={this.state.dataURI}/>
       )
     }
   },
-  render: function() {
+  render: function () {
     return (
       <div className="reactomeImage">
         {this.renderImg()}
