e8d07c1e9dbbcec3c0918479664881d7bbda649b
Joe Mulvaney <jmulvane@cshl.edu>
2016-03-29 11:54:22 -0400

    style pulled out of bundle.js into style.css

--- scripts/babel.js
+++ scripts/babel.js
@@ -5,23 +5,31 @@
 require('babel-register')({
   presets: ['es2015', 'react'],
   ignore: function(filename) {
-    var result;
-    if(filename.match(/gramene\-\w+\-vis/)) {
-      result = filename.match(/\.less$/);
-    }
-    else {
-      result = filename.indexOf('gramoogle/scripts') == -1;
-    }
-
+    var result = filename.indexOf('gramoogle/scripts') == -1
+      && filename.indexOf('gramene-genetree-vis/src') == -1
+      && filename.indexOf('gramene-search-vis/kb') == -1
+      // && filename.indexOf('babel-helper-builder-react-jsx') == -1;
     console.log(result, filename);
     return result;
   }
+  // ignore: function(filename) {
+  //   var result;
+  //   // if(filename.match(/gramene\-\w+\-vis/)) {
+  //   //   result = filename.match(/\.less$/);
+  //   // }
+  //   // else {
+  //     result = filename.indexOf('gramoogle/scripts') > -1
+  //       || filename.indexOf('gramene-') > -1;
+  //   // }
+  //
+  //   console.log(result, filename);
+  //
+  //   return result;
+  // }
 });
 
 global.window = {};
 global.navigator = {userAgent: 'node'};
-
-console.log(navigator);
 
 var React = require('react');
 var ReactDOMServer = require('react-dom/server');
