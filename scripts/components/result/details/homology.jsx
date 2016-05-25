fb1ea4c70d42a2b1b9170cd6fcd7f7bc016df72f
Andrew Olson <olson@cshl.edu>
2016-05-25 16:29:45 -0400

    update gramene-genetree-vis when taxa changes

--- scripts/components/result/details/homology.jsx
+++ scripts/components/result/details/homology.jsx
@@ -23,6 +23,9 @@ export default class Homology extends React.Component {
     this.unsubscribe = lutStore.listen((lutState) =>
       this.setState({taxonomy: lutState.taxonomy})
     );
+    this.unsubscribeFromSearchStore = searchStore.listen((searchState) =>
+      this.setState({genomesOfInterest: searchState.global.taxa})
+    );
 
     this.orthologs = this.orthologList();
     this.paralogs = this.paralogList();
@@ -35,6 +38,7 @@ export default class Homology extends React.Component {
 
   componentWillUnmount() {
     this.unsubscribe();
+    this.unsubscribeFromSearchStore();
     DocActions.noLongerNeedDocs('genetrees', this.treeId);
   }
 
