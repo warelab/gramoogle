743e8978495ed2693c7c682dcb8c5802d434f491
Joseph Mulvaney <me@mycro.be>
2016-06-14 21:23:06 -0400

    fix image locations

--- scripts/components/welcome/GrameneTools.jsx
+++ scripts/components/welcome/GrameneTools.jsx
@@ -29,31 +29,31 @@ const GrameneTools = () =>
         <GrameneTool title="Gramene Search"
                      description="Find and explore all data in Gramene database"
                      link={{onClick: focusSearch, href: 'javascript:(function noop(){})()'}}
-                     imgSrc="/assets/images/gramene_leaves.svg"/>
+                     imgSrc="assets/images/gramene_leaves.svg"/>
         <GrameneTool title="Genome Browser"
                      description="We share the Ensembl infrastructure published by the EBI"
                      link={{href: "http://ensembl.gramene.org/genome_browser/index.html"}}
-                     imgSrc="/assets/images/welcome/genomes.png"/>
+                     imgSrc="assets/images/welcome/genomes.png"/>
         <GrameneTool title="Plant Reactome"
                      description="Browse metabolic & regulatory pathways"
                      link={{href: "http://plantreactome.gramene.org/"}}
-                     imgSrc="/assets/images/welcome/pathways.png"/>
+                     imgSrc="assets/images/welcome/pathways.png"/>
         <GrameneTool title="BLAST"
                      description="Query our genomes with a DNA or protein sequence of interest"
                      link={{href: "http://ensembl.gramene.org/Tools/Blast?db=core"}}
-                     imgSrc="/assets/images/welcome/BLAST.png"/>
+                     imgSrc="assets/images/welcome/BLAST.png"/>
         <GrameneTool title="BioMart"
                      description="An advanced query interface"
                      link={{href: "http://www.gramene.org/biomart/martview"}}
-                     imgSrc="/assets/images/welcome/mart.png"/>
+                     imgSrc="assets/images/welcome/mart.png"/>
         <GrameneTool title="Bulk Downloads"
                      description="FTP download of our data"
                      link={{href: "http://gramene.org/ftp-download"}}
-                     imgSrc="/assets/images/welcome/genomes.png"/>
+                     imgSrc="assets/images/welcome/genomes.png"/>
         <GrameneTool title="Archive"
                      description="Legacy tools and data"
                      link={{href: "http://gramene.org/archive"}}
-                     imgSrc="/assets/images/welcome/genomes.png"/>
+                     imgSrc="assets/images/welcome/genomes.png"/>
       </ListGroup>
     </div>;
 export default GrameneTools;
