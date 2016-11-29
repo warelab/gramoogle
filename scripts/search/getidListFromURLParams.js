20044d6cb8187bb10b6b58eac9df7fab6ea2c84e
Andrew Olson <olson@cshl.edu>
2016-11-29 12:27:04 -0500

    trim version numbers from idList entries

--- scripts/search/getidListFromURLParams.js
+++ scripts/search/getidListFromURLParams.js
@@ -1,9 +1,13 @@
 import _ from "lodash";
 
+function trimVersion(id) {
+  return id.replace(/\.\d+$/,'');
+}
+
 export default function getidListFromURLParams() {
   const params = _.get(global.gramene, 'searchParams');
   if (params && params.idList) {
-    return _.uniq(params.idList.split(','));
+    return _.uniq(_.map(params.idList.split(','), trimVersion));
   }
   return [];
 }
