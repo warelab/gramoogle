4106b52edd29aeac787e8ef39d3d8e017c122a10
Andrew Olson <olson@cshl.edu>
2017-01-31 12:27:53 -0500

    show drupal web_widget in iframe

--- scripts/components/DrupalPage.jsx
+++ scripts/components/DrupalPage.jsx
@@ -1,46 +1,52 @@
 import React from 'react';
 import axios from "axios";
+import Spinner from './Spinner.jsx';
 
 
 export default class DrupalPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: null
+      aliases: {}
     };
   }
 
   componentDidMount() {
-    this.fetchPage()
-  }
-
-  componentDidUpdate (prevProps) {
-    let oldNode = prevProps.params.drupalPath || prevProps.params.drupalNode;
-    let newNode = this.props.params.drupalPath || this.props.params.drupalNode;
-    if (newNode !== oldNode)
-      this.fetchPage();
+    this.fetchAliases();
   }
 
   componentWillUnmount () {
     this.ignoreLastFetch = true;
   }
 
-  fetchPage () {
-    let drupal = this.props.params.drupalPath || `node/${this.props.params.drupalNode}`;
-    let url = `http://data.gramene.org/drupal/${drupal}`;
+  fetchAliases () {
+    let url = `/aliases`;
     axios.get(url).then(response => {
       if (!this.ignoreLastFetch) {
-        const title = response.data.title;
-        const body = response.data.body.und[0].safe_value;
-        const content = `<h3>${title}</h3><div>${body}</div>`;
-        this.setState({ page: content });
+        this.setState({ aliases: response.data });
       }
     })
   }
 
   render() {
-    return (
-      <div dangerouslySetInnerHTML={{__html:this.state.page}}></div>
-    );
+    let nid = null;
+    if (this.props.params.drupalPath) {
+      if (this.state.aliases[this.props.params.drupalPath]) {
+        nid = this.state.aliases[this.props.params.drupalPath];
+      }
+    }
+    else if (this.props.params.drupalNode) {
+      nid = this.props.params.drupalNode;
+    }
+    if (nid) {
+      return (
+        <iframe src={`http://www.gramene.org/ww?nid=${nid}`} frameBorder="0" width="100%" height="650px">
+          <p>browser doesn't support iframes</p>
+        </iframe>
+      );
+    }
+    else {
+      return <Spinner />;
+    }
   }
 }
--- server.js
+++ server.js
@@ -1,20 +1,52 @@
-var express = require('express')
-var path = require('path')
-var compression = require('compression')
+var express = require('express');
+var path = require('path');
+var compression = require('compression');
+var schedule = require('node-schedule');
+var mysql = require('mysql');
+var argv = require('minimist')(process.argv.slice(2));
 
-var app = express()
+var app = express();
 
-app.use(compression())
+app.use(compression());
+
+
+var aliasLUT = {};
+
+function updateLUT() {
+  var drupalDb = mysql.createConnection({
+    host: argv.h,
+    user: argv.u,
+    password: argv.p,
+    database: argv.d
+  });
+  drupalDb.query("select source, alias from url_alias", function(err, rows, fields) {
+    if (err) throw err;
+    rows.forEach(function(row) {
+      aliasLUT[row.alias] = row.source.replace(/node\//,'');
+    })
+  });
+  drupalDb.end();
+}
+
+updateLUT();
+var updateJob = schedule.scheduleJob({minute:[0,5,10,15,20,25,30,35,40,45,50,55]}, updateLUT);
+
 
 // serve our static stuff like index.css
 app.use(express.static(path.join(__dirname, 'build')))
 
+app.get('/aliases', function (req, res) {
+  res.json(aliasLUT)
+});
+
 // send all requests to index.html so browserHistory works
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+});
 
-var PORT = process.env.PORT || 8080
+var PORT = process.env.PORT || 8080;
+
 app.listen(PORT, function() {
   console.log('Production Express server running at localhost:' + PORT)
-})
+});
+
