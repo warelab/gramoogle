4079b99ce02ab0b9454e6d46b5d20464276fb3e4
Andrew Olson <olson@cshl.edu>
2019-06-05 14:29:03 -0400

    add tabs to the Ontology results summary component for table and graph views

--- scripts/components/results/Ontology.jsx
+++ scripts/components/results/Ontology.jsx
@@ -10,13 +10,16 @@ import _ from 'lodash';
 import cytoscape from 'cytoscape';
 import CytoscapeComponent from 'react-cytoscapejs'
 import dagre from 'cytoscape-dagre';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import ReactTable from 'react-table';
+
 cytoscape.use(dagre);
 
 
 export default class Ontology extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { tabIndex: 0 };
   }
 
   componentWillMount() {
@@ -36,8 +39,7 @@ export default class Ontology extends React.Component {
     let nodes = [];
     let edges = [];
     docs.forEach(d => {
-      let result = {};
-      result = {id: d._id, name: d.id, label: d.name, count: this.state.terms.data[d._id].count}
+      let result = {id: d._id, name: d.id, label: d.name, count: this.state.terms.data[d._id].count, definition: d.def};
       nodes.push({data: result});
       if(d.is_a) {
         d.is_a.forEach(e => {
@@ -54,42 +56,84 @@ export default class Ontology extends React.Component {
     if (this.state.terms && !prevState.terms) {
       console.log('requesting term ancestors from docstore',this.state.terms);
       var idList = this.state.terms.sorted.map(term => term.id);
-      DocActions.needDocs(this.props.collection, idList, null, ids => this.buildHierarchy(ids), {fl:'_id,id,name,description,is_a',rows:-1});
+      DocActions.needDocs(this.props.collection, idList, null, ids => this.buildHierarchy(ids), {fl:'_id,id,name,def,is_a',rows:-1});
     }
+  }
+  renderTable() {
+    let tableData = this.state.nodes.map(node => node.data);
+    return (
+      <ReactTable
+        data={tableData}
+        columns={[
+          {
+            Header: 'Id',
+            accessor: 'name'
+          },
+          {
+            Header: 'Number of Genes',
+            accessor: 'count'
+          },
+          {
+            Header: 'Name',
+            accessor: 'label'
+          },
+          {
+            Header: 'Definition',
+            accessor: 'definition'
+          }
+        ]}
+      />
+    )
+  }
+  renderGraph() {
+    return (
+      <CytoscapeComponent
+        cy = {cy => {
+          this.cy = cy.fit();
+          this.cy.layout({name: 'dagre', nodeDimensionsIncludeLabels: true, rankDir: 'BT'}).run();
+        }}
+        elements = {CytoscapeComponent.normalizeElements({nodes:this.state.nodes, edges:this.state.edges})}
+        stylesheet = {[
+          {
+            selector: 'edge',
+            style: {
+              'curve-style': 'bezier',
+              'target-arrow-shape': 'triangle',
+              'width': '2px'
+            }
+          },
+          {
+            selector: 'node',
+            style: {
+              'label': 'data(label)',
+              'text-max-width': '100px',
+              'text-wrap': 'wrap',
+              'font-size': '16px',
+              'background-color': 'mapData(count, 0, 100, blue, red)',
+              'font-family': 'Helvetica'
+            }
+          }
+        ]}
+        style = { {width: '1000px', height: '700px'}}
+      />
+    )
   }
   render() {
     return (
       <div>
         <h1>{this.props.name}</h1>
-        {/*<pre>{JSON.stringify(this.props,null,2)}</pre>*/}
-        {/*<pre>{this.state.terms && JSON.stringify(this.state.terms,null,2)}</pre>*/}
-        <CytoscapeComponent
-          cy = {cy => {this.cy = cy.fit();
-                this.cy.layout({name: 'dagre', nodeDimensionsIncludeLabels: true, rankDir: 'BT'}).run();}}
-          elements = {CytoscapeComponent.normalizeElements({nodes:this.state.nodes, edges:this.state.edges})}
-          stylesheet = {[
-            {
-              selector: 'edge',
-              style: {
-                'curve-style': 'bezier',
-                'target-arrow-shape': 'triangle',
-                'width': '2px'
-              }
-            },
-            {
-              selector: 'node',
-              style: {
-                'label': 'data(label)',
-                'text-max-width': '100px',
-                'text-wrap': 'wrap',
-                'font-size': '16px',
-                'background-color': 'mapData(count, 0, 100, blue, red)',
-                'font-family': 'Helvetica'
-              }
-            }
-          ]}
-          style = { {width: '1000px', height: '700px'}}/>
-        // <pre>{this.state.hierarchy && JSON.stringify(this.state.hierarchy, null, 2)}</pre>
+        <Tabs selectedIndex={this.state.tabIndex} onSelect={tabIndex => this.setState({ tabIndex })}>
+          <TabList>
+            <Tab>Table</Tab>
+            <Tab>Graph</Tab>
+          </TabList>
+          <TabPanel>
+            { this.state.nodes && this.renderTable() }
+          </TabPanel>
+          <TabPanel>
+            { this.state.nodes && this.renderGraph() }
+          </TabPanel>
+        </Tabs>
       </div>
     );
   }
